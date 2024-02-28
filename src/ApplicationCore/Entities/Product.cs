82ed7f77d3f1ea948cdd7aae616c7603c94f6506
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-02-28 12:36:36 +0300

    In the ApplicationCore project layer, the Entities and Interfaces folders have been created. Within the Entities folder, four main entity classes have been defined: Base Entity, Category, Brand, and Product, representing database tables. Relationships between these entities have been established.
    Additionally, an interface named IRepository has been added to the Interfaces folder. This interface defines basic CRUD operations. The purpose is to utilize the Generic Repository pattern, allowing for generalized database operations. By implementing this interface, classes responsible for database operations can adhere to a common structure, reducing code duplication and enhancing reusability.

---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string? PictureUri { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; } = null!;
+
+    }
+}
