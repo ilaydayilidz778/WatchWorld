b4180538d8363076c503db3175877ae4cf112935
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-03-04 10:44:27 +0300

    "In the Web Project layer, 'CategoryItemViewModel' and 'HomeViewModel' were added to the 'Models' folder. Additionally, 'Interfaces' and 'Services' folders were created, and the 'IHomeViewModelService' interface was defined. This interface was implemented in the 'HomeModelService' class in the 'Services' folder. In the 'ApplicationCore' project layer, a 'Specifications' folder was created, and a 'CategoryFilterSpecification' class inheriting from 'Specification<Product>' was generated."

---
+++ src/Web/Models/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Models
+{
+    public class HomeViewModel
+    {
+        public List<CatalogItemViewModel> CatologItems { get; set; } = new List<CatalogItemViewModel>();
+
+        public List<SelectListItem>? Brands { get; set; } 
+
+        public List<SelectListItem>? Categories { get; set; } 
+
+        public int? CategoryId { get; set; }
+
+        public int? BrandId { get; set; }
+    }
+}
---
+++ src/Web/Services/HomeViewModelService.cs
@@ -0,0 +1,48 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Web.Interfaces;
+using Web.Models;
+
+namespace Web.Services
+{
+    public class HomeViewModelService : IHomeViewModelService
+    {
+        private readonly IRepository<Product> _productRepository;
+        private readonly IRepository<Category> _categoryRepository;
+        private readonly IRepository<Brand> _brandRepository;
+
+        public HomeViewModelService(IRepository<Product> productRepository, IRepository<Category> categoryRepository, IRepository<Brand> brandRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _brandRepository = brandRepository;
+        }
+        public async Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId)
+        {
+            var specProducts = new CatalogFilterSpecification(categoryId, brandId);
+            var products = await _productRepository.GetAllAsync(specProducts);  
+
+            var vm = new HomeViewModel()
+            {
+                BrandId = brandId,
+                CategoryId = categoryId,
+                CatologItems = products.Select(x => new CatalogItemViewModel() 
+                { 
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    PictureUri = x.PictureUri ?? "noimage.jpg"
+                }
+                ).ToList(),
+                Brands = (await _productRepository.GetAllAsync()).Select(x =>
+                 new SelectListItem(x.Name, x.Id.ToString())).ToList(),
+                Categories = (await _categoryRepository.GetAllAsync()).Select(x =>
+                 new SelectListItem(x.Name, x.Id.ToString())).ToList()
+            };
+
+            return vm;
+        }
+    }
+}
